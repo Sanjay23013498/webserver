5d2b589952f8fec34b902967e58b6328e25e0d64
Sanjay23013498 <sanjaychinnadurai854@gmail.com>
2023-11-21 14:37:38 +0530

    success

--- webserver.py
+++ webserver.py
@@ -1,21 +1,91 @@
-from http.server import HTTPServer,BaseHTTPRequestHandler
+<tr>
+    <th align="center" bgcolor="yellow">8-10</th>
+    <td align="center" bgcolor="cyan" colspan="3">FREE SLOT</td>
+    <td align="center" bgcolor="cyan">PHY</td>
+    <td align="center" bgcolor="cyan">CHE</td>
+</tr>
 
-content='''
-<!doctype html>
-<html>
-<head>
-<title> My Web Server</title>
-</head>
-<body>
-<h1>Top Five Web Application Development Frameworks</h1>
-<h2>1.Django</h2>
-<h2>2. MEAN Stack</h2>
-<h2>3. MERN Stack </h2>
-<h2>4. ASP.NET core </h2>
-<h2>5. Spring </h2>
-</body>
-</html>
-'''
+<tr>
+    <th align="center" bgcolor="yellow">10-12</th>
+    <td align="center" bgcolor="cyan">GER</td>
+    <td align="center" bgcolor="cyan">CHE</td>
+</tr>
+
+<tr>
+    <th align="center" bgcolor="yellow">10-12</th>
+    <td align="center" bgcolor="cyan">GER</td>
+    <td align="center" bgcolor="cyan">FREE SLOT</td>
+    <td align="center" bgcolor="cyan">FWAD</td>
+    <td align="center" bgcolor="cyan">FWAD</td>
+    <td align="center" bgcolor="cyan">PHY</td>
+</tr>
+
+<tr>
+    <th align="center" bgcolor="yellow">12-1</th>
+    <td align="center" bgcolor="cyan" colspan="5">LUNCH</td>
+</tr>
+
+<tr>
+    <th align="center" bgcolor="yellow">1-3</th>
+    <td align="center" bgcolor="cyan" colspan="2">FREE SLOT</td>
+    <td align="center" bgcolor="cyan">MAT</td>
+    <td align="center" bgcolor="cyan">MAT</td>
+    <td align="center" bgcolor="cyan">SS</td>
+</tr>
+
+<tr>
+    <th align="center" bgcolor="yellow">3-5</th>
+    <td align="center" bgcolor="cyan" colspan="2">FREE SLOT</td>
+    <td align="center" bgcolor="cyan">GER</td>
+    <td align="center" bgcolor="cyan">CHE</td>
+    <td align="center" bgcolor="cyan">FWAD</td>
+</tr>
+</table>
+
+<table border="3" width="600" cellspacing='3' cellpaddling='3'>
+
+<tr>
+    <th align="center">S.No</th>
+    <th align="center">SUBJECT CODE</th> 
+    <th align="center">Subject Name</th>
+</tr>
+
+<tr>
+    <td align="center">1</td>
+    <td align="center">19A1414</td>
+    <td align="centre">Fundamentals of Web Application Devlopment(FWAD)</td>
+</tr>
+
+<tr>
+    <td align="center">2</td>
+    <td align="center">19EN612</td>
+    <td align="centre">German Basics (GER)</td>
+</tr>
+
+<tr>
+    <td align="center">3</td>
+    <td align="center">19PH206</td>
+    <td align="centre">Physics for Information Technology</td>
+</tr>
+
+<tr>
+    <td align="center">4</td> 
+    <td align="center">19CY205</td>
+    <td align="centre">Principles of Chemistry in Engineering(CHE)</td>
+</tr>
+
+<tr>
+    <td align="center">5</td>
+    <td align="center">19MA201</td>
+    <td align="centre">Calculus and Matrix Algebra (MAT)</td>
+</tr>
+
+<tr>
+    <td align="center">6</td>
+    <td align="center">19EY701</td>
+    <td align="center">soft skills(SS)</td>
+    </tr>
+
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
